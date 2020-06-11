9c6e0c44f71e487ba324c7cc39597eb0d45ba2d7
Michael Danko <michaeldanko@icloud.com>
2020-06-11 19:23:18 -0400

    Added project filesø

---
+++ Furiends/Furiends/FriendListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Furiends
+//
+//  Created by Michael Danko on 6/11/20.
+//  Copyright © 2020 Michael Danko. All rights reserved.
+//
+
+import SwiftUI
+
+struct tatiew: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendListView()
+    }
+}
