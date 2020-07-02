b72d522539d71d70a9ca07c32de41d17a1b2cb1c
Michael Danko <michaeldanko@icloud.com>
2020-07-01 21:46:48 -0400

    Reworks initial design of the AddFriendView Form layout

--- Furiends/Furiends/Views/AddFriendView.swift
+++ Furiends/Furiends/Views/AddFriendView.swift
@@ -14,33 +14,77 @@ struct AddFriendView: View {
     
     @State private var newPetName = ""
     @State private var newPetBreed = ""
+    @State private var newPetOwnerName = ""
     @Environment(\.presentationMode) var isPresented
     
     var body: some View {
-        Form {
-            VStack {
-                TextField("Pet Name", text: $newPetName)
-                TextField("Breed", text: $newPetBreed)
-                Button(action: saveFriend) {
-                    ZStack {
-                        Color(.systemTeal)
-                        Text("Save")
-                    }
+        
+        GeometryReader { geo in
+            Form {
+                
+                Section {
+                    Button(action: saveFriend, label: {
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Image(systemName: "plus.circle")
+                                    
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: geo.size.width / 7)
+                                Text("Add Picture")
+                                    .font(.headline)
+                            }
+                            Spacer()
+                        }
+                    })// End of Button
+                    
+                    
                 }
-            }
-        }
-    }
+                
+                Section(header: Text("Pet Details")) {
+                    TextField("Name", text: $newPetName)
+                    TextField("Breed", text: $newPetBreed)
+                    
+//MARK: TODO Add SegmentedPickerStyle
+//Seems like a bug in SegmentedPickerStyle()
+//
+//                    TextField("Behavior")
+//                    Picker("Pet Behavior", selection: $petBehaviorSelection) {
+//                        ForEach(behaviors, id: \.self) { selection in
+//                            Text(selection)
+//                        }
+//                    }
+//                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                
+                Section(header: Text("Owner Details")) {
+                    TextField("Name", text: $newPetOwnerName)
+                }
+                
+                Section {
+                    
+                    Button(action: saveFriend) {
+                        Text("Save Furiend")
+                            .multilineTextAlignment(.center)
+                            .frame(minWidth: 0, maxWidth: .infinity)
+                    }
+                    
+                }// End of Section
+            }// End of Form
+        }// End of GeometryReader geo
+    }// End of body
     
     func saveFriend() {
-        print("Going to save a new friend!")
         print(FriendDataModel.petList)
-        FriendDataModel.addFriend(name: newPetName, breed: newPetBreed)
-        print("Saved a new friend!")
-        print(FriendDataModel.petList)
+        FriendDataModel.addFriend(name: newPetName, breed: newPetBreed, owner: newPetOwnerName)
+
         
         
         self.newPetName = ""
         self.newPetBreed = ""
+        self.newPetOwnerName = ""
         self.isPresented.wrappedValue.dismiss()
     }
     
