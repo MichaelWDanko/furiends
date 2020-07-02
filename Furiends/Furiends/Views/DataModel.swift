ab1144f312f1c55fd31fb964374fc149c4ac7f97
Michael Danko <michaeldanko@icloud.com>
2020-07-01 21:44:32 -0400

    Adds owner to the struct Pet

--- Furiends/Furiends/Views/DataModel.swift
+++ Furiends/Furiends/Views/DataModel.swift
@@ -12,20 +12,22 @@ struct Pet: Identifiable {
     var id = UUID().uuidString
     var name: String
     var breed: String
+    var owner: String
 }
 
 class FuriendModel: ObservableObject {
     
     @Published var petList = [Pet]()
     
-    func addFriend(name: String, breed: String) {
-        petList.append(Pet(name: name, breed: breed))
+    
+    func addFriend(name: String, breed: String, owner: String) {
+        petList.append(Pet(name: name, breed: breed, owner: owner))
     }
     
     init() {
-        petList.append(Pet(name: "Woofer", breed: "Dawg"))
-        petList.append(Pet(name: "Floofer", breed: "Dawg"))
-        petList.append(Pet(name: "Jax", breed: "Dawg"))
+        petList.append(Pet(name: "Woofer", breed: "Dawg", owner: "Human"))
+        petList.append(Pet(name: "Floofer", breed: "Dawg", owner: "Human"))
+        petList.append(Pet(name: "Jax", breed: "Dawg",  owner: "Human"))
 //        petList.append(Pet(name: "Dooper", breed: "Dawg"))
 //        petList.append(Pet(name: "Booper", breed: "Dawg"))
     }
