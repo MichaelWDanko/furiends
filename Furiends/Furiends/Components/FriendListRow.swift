e332c394fab2de145c1704922ed4d097826a7dff
Michael Danko <michaeldanko@icloud.com>
2020-07-03 08:29:23 -0400

    Reworks the FriendListView to not have an image and to use the gender specific color dots

--- Furiends/Furiends/Components/FriendListRow.swift
+++ Furiends/Furiends/Components/FriendListRow.swift
@@ -9,74 +9,119 @@
 import SwiftUI
 
 struct FriendListRow: View {
-    @State private var isExpanded = false
-    @Namespace private var animation
-    var name =  ""
-    var breed = ""
     
+    var name: String
+    var breed: String
+    var gender: PetGender
     var screenWidth: CGFloat
-    var maxWidth: CGFloat {
-        isExpanded ? screenWidth / 4 : screenWidth / 2
+    
+    var genderColor: Color {
+        gender == PetGender.male ? Color("MaleColor") : Color("FemaleColor")
     }
-    var randomPic = Int.random(in: 1...2)
-   
+    
     var body: some View {
-    
-        Group {
-            if self.isExpanded {
-                //Row is expanded. Need to show details beside picture.
+        ZStack {            
+            Rectangle()
+                .cornerRadius(10)
+                .foregroundColor(Color("Primary"))
+            
+            HStack {
                 
-                HStack {
-//                    Spacer()
-                    Image("dog\(self.randomPic)")
-                        .resizable()
-                        .furiendImage()
-                        .frame(width: maxWidth, height: maxWidth)
-                        .matchedGeometryEffect(id: "FuriendImage", in: self.animation)
-//                    Spacer()
-                    VStack {
-                        Text(self.name).font(.title)
-                            .furiendName()
-                        Text(self.breed).font(.headline)
-                            .animation(.easeIn)
-                    }
-                    .foregroundColor(.primary)
-                    Spacer()
-                    
-                }// End of HStack
-//                .animation(.easeOut(duration: 1.0))
-                .matchedGeometryEffect(id: "DetailsContainer", in: self.animation)
-                .onTapGesture {
-                    self.isExpanded.toggle()
-                }
-                
-            } else {
-                //Row is not expanded. Need to show name below picture.
-                
+                Circle()
+                    .foregroundColor(genderColor)
+                    .padding(.top, 5)
+                    .padding(.bottom, 5)
+                    .padding(.leading, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .frame(width: screenWidth / 5, height: screenWidth / 5)
+                Spacer()
                 VStack {
-                    
-                    Image("dog\(self.randomPic)")
-                        .resizable()
-                        .furiendImage()
-                        .frame(width: maxWidth, height: maxWidth)
-                        .matchedGeometryEffect(id: "FuriendImage", in: self.animation)
-                    
-                    Text(self.name)
-                        .furiendName()
-                    
-               }//End of VStack
-//                .animation(.easeOut(duration: 1.0))
-                .matchedGeometryEffect(id: "DetailsContainer", in: self.animation)
-                .onTapGesture {
-                    self.isExpanded.toggle()
-                    
+                    Text(name)
+                        .foregroundColor(Color("PrimaryFont"))
+                        .font(.title)
+                        .frame(minWidth: 0, maxWidth: .infinity)
                 }
-                
-            }// End of If/Else
-        }
-    }// End of body
+                .padding(.all, 10)
+            }
+            
+        }// End of ZStack
+    }
     
-}// End of FriendView
+    
+}
+
+
+
+
+
+//struct FriendListRow: View {
+//    @State private var isExpanded = false
+//    @Namespace private var animation
+//    var name =  ""
+//    var breed = ""
+//
+//    var screenWidth: CGFloat
+//    var maxWidth: CGFloat {
+//        isExpanded ? screenWidth / 4 : screenWidth / 2
+//    }
+//    var randomPic = Int.random(in: 1...2)
+//
+//    var body: some View {
+//
+//        Group {
+//            if self.isExpanded {
+//                //Row is expanded. Need to show details beside picture.
+//
+//                HStack {
+////                    Spacer()
+//                    Image("dog\(self.randomPic)")
+//                        .resizable()
+//                        .furiendImage()
+//                        .frame(width: maxWidth, height: maxWidth)
+//                        .matchedGeometryEffect(id: "FuriendImage", in: self.animation)
+////                    Spacer()
+//                    VStack {
+//                        Text(self.name).font(.title)
+//                            .furiendName()
+//                        Text(self.breed).font(.headline)
+//                            .animation(.easeIn)
+//                    }
+//                    .foregroundColor(.primary)
+//                    Spacer()
+//
+//                }// End of HStack
+////                .animation(.easeOut(duration: 1.0))
+//                .matchedGeometryEffect(id: "DetailsContainer", in: self.animation)
+//                .onTapGesture {
+//                    self.isExpanded.toggle()
+//                }
+//
+//            } else {
+//                //Row is not expanded. Need to show name below picture.
+//
+//                VStack {
+//
+//                    Image("dog\(self.randomPic)")
+//                        .resizable()
+//                        .furiendImage()
+//                        .frame(width: maxWidth, height: maxWidth)
+//                        .matchedGeometryEffect(id: "FuriendImage", in: self.animation)
+//
+//                    Text(self.name)
+//                        .furiendName()
+//
+//               }//End of VStack
+////                .animation(.easeOut(duration: 1.0))
+//                .matchedGeometryEffect(id: "DetailsContainer", in: self.animation)
+//                .onTapGesture {
+//                    self.isExpanded.toggle()
+//
+//                }
+//
+//            }// End of If/Else
+//        }
+//    }// End of body
+//
+//}// End of FriendView
 
 struct FuriendImage: ViewModifier {
     func body(content: Content) -> some View {
@@ -109,12 +154,8 @@ extension View {
     }
 }
 
-//
-//struct FriendView_Previews: PreviewProvider {
+//struct FriendListRow_Previews: PreviewProvider {
 //    static var previews: some View {
-//        GeometryReader { geo in
-//            FriendListRow(screenWidth: geo.size.width)
-//        }
-//
+//        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
 //    }
 //}
